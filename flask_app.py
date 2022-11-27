998b8a813da06462654ddff1742fc378b006aef8
nbossingham <95383785+nbossingham@users.noreply.github.com>
2022-11-27 18:52:00 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, make_response
 from flask_socketio import SocketIO, send
-
+import crypto
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.static_folder = 'static'
@@ -11,6 +11,7 @@ def encrypt(msg):
 
 @app.route('/')
 def home():
-    return render_template('test.html')
+    userPrivate,userPublicX,userPublicY,userSharedX,userSharedY,botPrivate,botPublicX,botPublicY,botSharedX,botSharedY = diffieHellmanECC()
+    return render_template('test.html',userPrivate=userPrivate,userPublicX=userPublicX,userPublicY=userPublicY,userSharedX=userSharedX,userSharedY=userSharedY,botPrivate=botPrivate,botPublicX=botPublicX,botPublicY=botPublicY,botSharedX=botSharedX,botSharedY=botSharedY)
 
 
