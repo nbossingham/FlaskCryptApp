7406c85b1e0310ddcc63dd60e0c7a9bbbaa1b76e
nbossingham <95383785+nbossingham@users.noreply.github.com>
2022-11-27 23:02:26 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, make_response
 from flask_socketio import SocketIO, send
-import os
-
+import random
 ## CURVE: 
 ##     y^2 = x^3 +2x + 2 mod 17
 ## BASE POINT:
@@ -23,7 +22,7 @@ y0 = 1
 
 def findModularInverse(a, mod):
 			
-	while(a < 0):
+	while(a <= 0):
 		a = a + mod
 	
 	#a = a % mod
@@ -31,7 +30,7 @@ def findModularInverse(a, mod):
 	x1 = 1; x2 = 0; x3 = mod
 	y1 = 0; y2 = 1; y3 = a
 	
-	q = int(x3 / y3)
+	q = int((x3/y3))
 	t1 = x1 - q*y1
 	t2 = x2 - q*y2
 	t3 = x3 - (q*y3)
@@ -54,17 +53,19 @@ def findModularInverse(a, mod):
 	return y2
 
 def pointAddition(x1, y1, x2, y2, a, b, mod):
-	
+	#print(f"x1:{x1},y1:{y1}")
+	#print(f"x2:{x2},y2:{y2}")
+	skip = False
 	if x1 == x2 and y1 == y2:
 		#doubling
 		beta = (3*x1*x1 + a) * (findModularInverse(2*y1, mod))
-	
 	else:
 		#point addition
+		#print(f"x1:{x1},y1:{y1}")
 		beta = (y2 - y1)*(findModularInverse((x2 - x1), mod))
-	
-	x3 = beta*beta - x1 - x2
-	y3 = beta*(x1 - x3) - y1
+	if skip==False:
+	    x3 = beta*beta - x1 - x2
+	    y3 = beta*(x1 - x3) - y1
 	
 	x3 = x3 % mod
 	y3 = y3 % mod
@@ -74,7 +75,7 @@ def pointAddition(x1, y1, x2, y2, a, b, mod):
 	
 	while(y3 < 0):
 		y3 = y3 + mod
-	
+	#print(f"x3:{x3},y3:{y3}")
 	return x3, y3
 
 def applyDoubleAndAddMethod(x0, y0, k, a, b, mod):
@@ -82,19 +83,22 @@ def applyDoubleAndAddMethod(x0, y0, k, a, b, mod):
 	x_temp = x0
 	y_temp = y0
 	
-	kAsBinary = bin(k) #0b1111111001
-	kAsBinary = kAsBinary[2:len(kAsBinary)] #1111111001
-	#print(kAsBinary)
+	kAsBinary = bin(int(k))#0b1111111001
+	kAsBinary = kAsBinary[2:] #1111111001
+	#print(f"Numerical Key: {int(k,16)}")
+	#print(f"Binary Key: {kAsBinary}")
 	
 	for i in range(1, len(kAsBinary)):
 		currentBit = kAsBinary[i: i+1]
+		#print(f"{i}:")
 		#always apply doubling
+		
 		x_temp, y_temp = pointAddition(x_temp, y_temp, x_temp, y_temp, a, b, mod)
 		
 		if currentBit == '1':
 			#add base point
 			x_temp, y_temp = pointAddition(x_temp, y_temp, x0, y0, a, b, mod)
-	
+		#print(f"xt:{x_temp},yt:{y_temp}")
 	return x_temp, y_temp
 
 ## END QUOTED CODE
@@ -102,14 +106,28 @@ def applyDoubleAndAddMethod(x0, y0, k, a, b, mod):
 ## ECDH with 128 bit keys
 
 def diffieHellmanECC():
-    userPrivate = os.urandom(16).hex()
+    #os.urandom(16).hex()
+    userPrivate = random.randrange(1,order-1)
+    
     userPublicX,userPublicY = applyDoubleAndAddMethod(x0, y0, userPrivate, a, b, mod)
-
-    botPrivate = os.urandom(16).hex()
+    print(f"Private Key: {userPrivate}")
+    print(f"Numerical Key: {int(userPrivate)}")
+    print(f"pubx: {userPublicX},puby {userPublicY}")
+    
+    botPrivate = random.randrange(1,order-1)
+    
     botPublicX,botPublicY = applyDoubleAndAddMethod(x0, y0, botPrivate, a, b, mod)
+    print(f"Private Key: {botPrivate}")
+    print(f"Numerical Key: {int(botPrivate)}")
+    print(f"pubx: {botPublicX},puby {botPublicY}")
     
     userSharedX, userSharedY = applyDoubleAndAddMethod(botPublicX, botPublicY, userPrivate, a, b, mod)
+    print(f"pubx: {userSharedX},puby {userSharedY}")
+    
     botSharedX, botSharedY = applyDoubleAndAddMethod(userPublicX, userPublicY, botPrivate, a, b, mod)
+    print(f"pubx: {botSharedX},puby {botSharedY}")
+    
+    return userPrivate,userPublicX,userPublicY,userSharedX,userSharedY,botPrivate,botPublicX,botPublicY,botSharedX,botSharedY
 
 app = Flask(__name__)
 socketio = SocketIO(app)
